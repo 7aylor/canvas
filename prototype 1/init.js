df49f7271b1faa73d4958508c557e5f518e29bb8
Taylor <tbuchhei@gmail.com>
2018-10-21 17:00:59 -0700

    first dungeon. Player movement. Enemies. Key. Door

--- prototype 1/init.js
+++ prototype 1/init.js
@@ -1,17 +1,23 @@
-const CANVAS = document.getElementById("game");
-const CTX = CANVAS.getContext("2d");
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
 const TILE_WIDTH = 40;
 const TILE_HEIGHT = 40;
-const NUM_COLS = CANVAS.width/TILE_WIDTH;
-const NUM_ROWS = CANVAS.height/TILE_HEIGHT;
+const NUM_COLS = canvas.width/TILE_WIDTH;
+const NUM_ROWS = canvas.height/TILE_HEIGHT;
 const TILE_GROUND = 0;
 const TILE_WALL = 1;
 const TILE_PLAYER = 2;
-const imgPath = "art/img/";
+const TILE_ENEMY = 3;
+const TILE_DOOR = 4;
+const TILE_KEY = 5;
+const IMG_PATH = "art/img/";
+
 
 var map = [];
 var images = [];
 var imagesLoaded;
+var playerPos = {x: 0, y: 0};
+var playerHealth = 40;
 
 window.onload = function(){
     init();
@@ -20,12 +26,12 @@ window.onload = function(){
 //get canvas and ctx
 function init(){
     //if we can't get canvas context, user is on an unsupported browser
-    if(!CTX){
+    if(!ctx){
         document.createTextNode("Unsupported browser");
     }
     else{
-        CTX.fillStyle = "black";
-        CTX.fillRect(0, 0, CANVAS.width, CANVAS.height);
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         initTiles();
         loadImages();
     }
@@ -33,20 +39,30 @@ function init(){
 
 function startGame(){
     setInterval(update, 1000/30); //set fps to 30
+    setPlayerInput();
 }
 
 function loadImages(){
 
-    var imagePaths = [imgPath + "ground.png", 
-                      imgPath + "wall.png",
-                      imgPath + "player.png"];
+    var imagePaths = [IMG_PATH + "ground.png", 
+                      IMG_PATH + "wall.png",
+                      IMG_PATH + "player.png",
+                      IMG_PATH + "enemy.png",
+                      IMG_PATH + "door.png",
+                      IMG_PATH + "key.png"];
 
     imagesLoaded = imagePaths.length - 1;
 
     for(var i = 0; i < imagePaths.length; i++){
         var img = new ImageClass(imagePaths[i], i);
     }
+}
 
+function checkImagesLoaded(){
+    imagesLoaded--;
+    if(imagesLoaded == 0){
+        startGame();
+    }
 }
 
 function initTiles(){
@@ -64,15 +80,23 @@ function initTiles(){
     }
 
     map[5][5] = TILE_PLAYER;
+    map[8][8] = TILE_ENEMY;
+    map[2][13] = TILE_ENEMY;
+    map[0][6] = TILE_DOOR;
+    map[12][6] = TILE_KEY;
 }
 
 function drawTiles(){
     for(var x = 0; x < NUM_COLS; x++){
         for(var y = 0; y < NUM_ROWS; y++){
-            if(map[x][y] == TILE_PLAYER){
-                CTX.drawImage(images[TILE_GROUND], x * 40, y * 40);
+            if(map[x][y] != TILE_WALL && map[x][y] != TILE_DOOR){
+                ctx.drawImage(images[TILE_GROUND], x * 40, y * 40);
             }
-            CTX.drawImage(images[map[x][y]], x * 40, y * 40);
+            if(map[x][y] == TILE_PLAYER){
+                playerPos.x = x;
+                playerPos.y = y;
+            }
+            ctx.drawImage(images[map[x][y]], x * 40, y * 40);
         }
     }
 }
@@ -88,10 +112,10 @@ function ImageClass(path, tileType){
     this.loadImage = checkImagesLoaded();
 }
 
-function checkImagesLoaded(){
-    console.log(imagesLoaded);
-    imagesLoaded--;
-    if(imagesLoaded == 0){
-        startGame();
+function initEnemies(){
+    var numEnemies = Math.floor(Math.random() * 8) + 1;
+
+    for(var i = 0; i < numEnemies; i++){
+
     }
 }
--- prototype 1/update.js
+++ prototype 1/update.js
@@ -1,3 +1,62 @@
 function update(){
     drawTiles();
+    drawUI();
+}
+
+function setPlayerInput(){
+    document.addEventListener("keydown", movePlayer);
+}
+
+function movePlayer(evt){
+    if(evt.keyCode == 87){ //w
+        updatePlayerLocationIfKeyPressed(0, -1);
+    }
+    if(evt.keyCode == 65){ //a
+        updatePlayerLocationIfKeyPressed(-1, 0);
+    }
+    if(evt.keyCode == 83){ //s
+        updatePlayerLocationIfKeyPressed(0, 1);
+    }
+    if(evt.keyCode == 68){ //d
+        updatePlayerLocationIfKeyPressed(1, 0);
+    }
+}
+
+function clearMapPos(x,y,tileType){
+    map[x][y] = tileType;
+}
+
+function updatePlayerLocationIfKeyPressed(deltaX, deltaY){
+    if(movePosValid(playerPos.x + deltaX, playerPos.y + deltaY)){
+        if(map[playerPos.x + deltaX][playerPos.y + deltaY] == TILE_ENEMY){
+            playerHealth -= 5;
+        }
+        clearMapPos(playerPos.x, playerPos.y, TILE_GROUND);
+        playerPos.x += deltaX;
+        playerPos.y += deltaY;
+        map[playerPos.x][playerPos.y] = TILE_PLAYER;
+        console.clear();
+        console.log("PlayerX: " + playerPos.x);
+        console.log("PlayerY: " + playerPos.y);
+    }
+}
+
+function movePosValid(newX, newY){
+    if(map[newX][newY] == TILE_DOOR){
+        initTiles();
+        return;
+    }
+    return map[newX][newY] != TILE_WALL;
+}
+
+function drawUI(){
+    drawPlayerHealth();
+}
+
+function drawPlayerHealth(){
+    //playerHealth = 30;
+    ctx.fillStyle = "#00ff00";
+    ctx.fillRect(playerPos.x * 40, playerPos.y * 40 - 3, 40, 2);
+    ctx.fillStyle = "#ff0000";
+    ctx.fillRect(playerPos.x * 40 + playerHealth, playerPos.y * 40 - 3, 40 - playerHealth, 2);
 }
