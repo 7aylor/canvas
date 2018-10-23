85761b3f09dc72444baec44ec9775319ac34d04e
Taylor <tbuchhei@gmail.com>
2018-10-23 08:02:06 -0700

    Started work on an inventory system

--- prototype 1/init.js
+++ prototype 1/init.js
@@ -11,13 +11,16 @@ const RIGHT_ARROW = 39;
 const DOWN_ARROW = 40;
 
 var tiles = {
-    ground: {val: 0, imgLoc: IMG_PATH + "ground.png"},
-    wall:   {val: 1, imgLoc: IMG_PATH + "wall.png"},
-    player: {val: 2, imgLoc: IMG_PATH + "player.png"},
-    enemy:  {val: 3, imgLoc: IMG_PATH + "enemy.png"},
-    door:   {val: 4, imgLoc: IMG_PATH + "door.png"},
-    key:    {val: 5, imgLoc: IMG_PATH + "key.png"},
+    ground: {val: 0 , imgLoc: IMG_PATH + "ground.png"},
+    wall:   {val: 1 , imgLoc: IMG_PATH + "wall.png"},
+    player: {val: 2 , imgLoc: IMG_PATH + "player.png"},
+    enemy:  {val: 3 , imgLoc: IMG_PATH + "enemy.png"},
+    door:   {val: 4 , imgLoc: IMG_PATH + "door.png"},
+    key:    {val: 5 , imgLoc: IMG_PATH + "key.png"},
+    bag:    {val: 6 , imgLoc: IMG_PATH + "bag.png"}
 }
+
+var inventory = [tiles.bag];
 
 console.log(Object.keys(tiles));
 
@@ -48,6 +51,7 @@ function init(){
 
 function startGame(){
     setInterval(update, 1000/30); //set fps to 30
+    drawInventory();
     setPlayerInput();
 }
 
@@ -58,13 +62,12 @@ function loadImages(){
     for(var t in tiles){
         var obj = tiles[t];
         var img = new ImageClass(obj.imgLoc);
-        console.log(t + ": " + obj.val);
     }
 }
 
 function checkImagesLoaded(){
     imagesLoaded--;
-    console.log(imagesLoaded);
+    
     if(imagesLoaded == 0){
         startGame();
     }
@@ -73,9 +76,9 @@ function checkImagesLoaded(){
 function initTiles(){
     for(var x = 0; x < NUM_COLS; x++){
         map[x] = new Array();
-        for(var y = 0; y < NUM_ROWS; y++){
+        for(var y = 0; y < NUM_ROWS - 1; y++){
             if(x == 0 || x == NUM_COLS - 1 ||
-               y == 0 || y == NUM_ROWS - 1){
+               y == 0 || y == NUM_ROWS - 2){
                 map[x][y] = tiles.wall.val//TILE_WALL;
             }
             else{
@@ -86,14 +89,23 @@ function initTiles(){
 
     map[5][5] = tiles.player.val//TILE_PLAYER;
     map[8][8] = tiles.enemy.val;//TILE_ENEMY;
-    map[2][13] = tiles.enemy.val;//TILE_ENEMY;
+    map[2][12] = tiles.enemy.val;//TILE_ENEMY;
     map[0][6] = tiles.door.val;//TILE_DOOR;
     map[12][6] = tiles.key.val;//TILE_KEY;
 }
 
+function drawInventory(){
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, (NUM_ROWS - 1) * 40, canvas.width, 40);
+    for(var x = 0; x < inventory.length; x++){
+        console.log(images[inventory[x].val]);
+        ctx.drawImage(images[inventory[x].val], 40, 40);
+    }
+}
+
 function drawTiles(){
     for(var x = 0; x < NUM_COLS; x++){
-        for(var y = 0; y < NUM_ROWS; y++){
+        for(var y = 0; y < NUM_ROWS - 1; y++){
             if(map[x][y] != tiles.wall.val && map[x][y] != tiles.door.val){
                 ctx.drawImage(images[tiles.ground.val], x * 40, y * 40);
             }
